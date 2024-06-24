cc2f77f2cc60ed3853a53a68f093bd652c951263
JiHongKim98 <kinjihong9598@gmail.com>
2024-06-24 23:17:27 +0900

    chore: Modify blog config to fit my blog settings

--- blog-config.js
+++ blog-config.js
@@ -1,23 +1,14 @@
 module.exports = {
   title: "JiHongKim98",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
+  description: "Hello :) This is JiHongKim98's tech blog",
   author: "JiHongKim98",
   siteUrl: "https://jihongkim98.netlify.app/",
   links: {
     github: "https://github.com/jihongkim98",
-    // linkedIn: "https://linkedin.com",
-    // facebook: "https://www.facebook.com",
-    // twitter: "https://twitter.com",
-    // x: "https://x.com",
-    // instagram: "https://www.instagram.com",
-    // medium: "https://medium.com",
-    // blogger: "https://blogger.com",
     email: "mailto:kinjihong9598@gmail.com",
-    // resume: "https://hudi.blog",
-    // link: "https://hudi.blog",
   },
   utterances: {
-    repo: "JiHongKim98/hong.log",
+    repo: "JiHongKim98/hong.dev",
     type: "pathname",
   },
 }
